90177de0b924268e638229238f52eb29e9125693
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-28 19:34:59 +0300

    Добавьте файлы проекта.

---
+++ CS17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int degree = 1;
+            int number = 2;
+            int maxRandomValue = 50;
+            int minRandomValue = 2;
+            int numberDegree;
+            int randomNumber = random.Next(minRandomValue, maxRandomValue);
+            numberDegree = number;
+            
+            while (numberDegree < randomNumber)
+            {
+                numberDegree *= number;
+                degree++;
+            }
+            Console.WriteLine($"Случайное число : {randomNumber} , минимальную степень двойки, превосходящую заданное число : {degree} , итоговое минимальное число : {numberDegree}");
+        }
+    }
+}
